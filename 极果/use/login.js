5184be5aa67ac794732f537dd4145a4f69c34bc3
Mumu00-a <1306924984@qq.com>
2020-11-19 14:07:48 +0800

    'uu'

--- 极果/use/login.js
+++ 极果/use/login.js
@@ -1,4 +1,9 @@
 //用户
+if (localStorage.getItem('miss') == 'true') {
+    $('#tel').val(localStorage.getItem('username'));
+    $('#password').val(localStorage.getItem('password'));
+    $('.loadGiet').is(':checked');
+}
 $('#loginform').on('submit', function () {
     $.ajax({
         url: 'http://192.168.1.64:3000/users/login',
@@ -10,7 +15,15 @@ $('#loginform').on('submit', function () {
         success: function (res) {
             console.log(res)
             if (res.msg == '登录成功！') {
-                window.open('index.html');
+                if ($('.loadGiet').is(':checked')) {
+                    localStorage.setItem('username', $('#tel').val());
+                    localStorage.setItem('password', $('#password').val());
+                    localStorage.setItem('miss',$('.loadGiet').is(':checked'))
+                } else {
+                    localStorage.clear();
+            
+                }
+                window.open('../index.html');
             }
         },
         dataType: 'json'
